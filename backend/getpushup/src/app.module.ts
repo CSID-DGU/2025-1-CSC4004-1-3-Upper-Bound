f282c5b0054b566f03dc84373ff9ad07a6ba7bd5
BAE-INHO <enobae0724@naver.com>
2025-05-02 12:39:28 +0900

    feat pushup

--- backend/getpushup/src/app.module.ts
+++ backend/getpushup/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnalysisModule } from './analysis/analysis.module';
+import { PushupModule } from './pushup/pushup.module';
 
 @Module({
-  imports: [AnalysisModule],
+  imports: [AnalysisModule, PushupModule],
   controllers: [AppController],
   providers: [AppService],
 })
