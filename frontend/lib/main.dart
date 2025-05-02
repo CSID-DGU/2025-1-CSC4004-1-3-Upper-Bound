56bea5b6cd08420516d71a81ad5aba27c63dca81
ehsui <sally1397@naver.com>
2025-05-02 14:08:17 +0900

    네비게이션 바 widget 분리

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
-import 'screens/main_screen.dart';
-import 'screens/records_screen.dart';
-import 'screens/profile_screen.dart';
+import 'widgets/root_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,60 +14,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: RootScreen(),
-    );
-  }
-}
-
-class RootScreen extends StatefulWidget {
-  @override
-  _RootScreenState createState() => _RootScreenState();
-}
-
-class _RootScreenState extends State<RootScreen> {
-  int _selectedIndex = 0;
-
-  final List<Widget> _screens = [
-    HomeScreen(),
-    MainScreen(),
-    RecordsScreen(),
-    ProfileScreen(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '홈',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.videocam),
-            label: '메인 기능',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            label: '월별 기록',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: '내 정보',
-          ),
-        ],
-      ),
     );
   }
 }
