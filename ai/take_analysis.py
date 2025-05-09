be0a72032d1c4178693586c9ca26ff1eb456f909
서한유 <hu8232@naver.com>
2025-05-09 11:39:21 +0900

    feat 지표시각화

--- ai/take_analysis.py
+++ ai/take_analysis.py
@@ -2,6 +2,12 @@ import cv2
 import mediapipe as mp
 import os
 import numpy as np
+import math
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from scipy.ndimage import gaussian_filter1d
 
 # MediaPipe Pose 초기화
 mp_pose = mp.solutions.pose # Pose 모듈 로딩
@@ -9,6 +15,31 @@ pose = mp_pose.Pose() # Pose 추정기 객체 생성
 mp_drawing = mp.solutions.drawing_utils # 랜드마크 시각화
 
 landmark_list = [] # landmark output
+
+# 각도 저장할 리스트
+elbow_angles = []
+hip_angles = []
+knee_angles = []
+lower_body_alignment = []
+elbow_x = []
+upper_arm_lengths = []
+forearm_lengths = []
+
+shox = []
+elbx = []
+wrix = []
+
+height1 = []
+# Pose landmarks 번호 (MediaPipe 기준)
+LEFT_EYE = 2
+LEFT_SHOULDER = 11
+LEFT_ELBOW = 13
+LEFT_WRIST = 15
+LEFT_HIP = 23
+LEFT_KNEE = 25
+LEFT_ANKLE = 27
+LEFT_HEEL = 29
+
 
 def detect_and_display(video_path): # landmark 추출
     cap = cv2.VideoCapture(video_path)
@@ -48,7 +79,10 @@ def detect_and_display(video_path): # landmark 추출
             for landmark in results.pose_landmarks.landmark:
                 row.extend([landmark.x, landmark.y, landmark.z, landmark.visibility])
         else:
-            row.extend([None] * 33 * 4)
+            if frame_idx-1 < 0:
+                row.extend(frame_idx-1)
+            else:
+                row.extend([0.0] * 33 * 4)
 
         landmark_list.append(row)
 
@@ -60,16 +94,125 @@ def detect_and_display(video_path): # landmark 추출
             break
 
     landmark_array = np.array(landmark_list)
-    print("Shape:", landmark_array.shape)
-    print("First 3 frames:\n", landmark_array[:3, :])
+    #print("Shape:", landmark_array.shape)
+    #print("First 3 frames:\n", landmark_array[:3, :])
 
     cap.release()
     cv2.destroyAllWindows()
 
+def calculate_angle(a, b, c):
+    a = np.array(a) 
+    b = np.array(b)
+    c = np.array(c)
+
+    # 벡터 구하기
+    ba = a - b
+    bc = c - b
+
+    # 코사인 각도 계산
+    cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
+    angle = np.arccos(cosine_angle)  # 라디안
+    return np.degrees(angle)  # degree로 변환
+
+def calculate_distance(a, b):
+    a = np.array(a)
+    b = np.array(b)
+    return np.linalg.norm(a - b)
+
+def get_coord(row, idx):
+    base = 1 + idx * 4
+    return [row[base], row[base + 1], row[base + 2]]
+
 def analysis():
-    None
+    for idx, row in enumerate(landmark_list):
+        shoulder = get_coord(row, LEFT_SHOULDER)
+        elbow = get_coord(row, LEFT_ELBOW)
+        wrist = get_coord(row, LEFT_WRIST)
+        hip = get_coord(row, LEFT_HIP)
+        knee = get_coord(row, LEFT_KNEE)
+        ankle = get_coord(row, LEFT_ANKLE)
+        eye = get_coord(row, LEFT_EYE)
+        heel = get_coord(row, LEFT_HEEL)   
+        #키
+        height = calculate_distance(heel, eye)
+        head = calculate_distance(shoulder, eye)
+        height1.append(height+head)
+
+        #전완과 팔꿈치 x 좌표 차이
+        elbow_x.append(elbow[0]-wrist[0])
+        #어깨 외전
+        upper_arm = calculate_distance(shoulder, elbow)
+        forearm = calculate_distance(elbow, wrist)
+        upper_arm_lengths.append(upper_arm)
+        forearm_lengths.append(forearm)
+
+        shox.append(shoulder[0])
+        elbx.append(elbow[0])
+        wrix.append(wrist[0])
+
+        #팔꿈치 가동범위
+        elbow_angle = calculate_angle(shoulder, elbow, wrist)
+        elbow_angles.append(elbow_angle)
+
+        #하체 정렬
+        hip_angle = calculate_angle(shoulder, hip, knee)
+        knee_angle = calculate_angle(hip, knee, ankle)
+        hip_angles.append(hip_angle)
+        knee_angles.append(knee_angle)
+        lower_body_alignment.append(180-(hip_angle+knee_angle)/2)
+        
+def plot_joint_angles():
+    frames = list(range(len(elbow_angles)))  # 프레임 번호 기준 x축
+
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(2, 2, 1)
+    plt.plot(frames, elbow_x, color='green')
+    plt.xlabel("Frame")
+    plt.ylabel("pixel")
+    plt.title("Elbow-Wrist aligment")
+    plt.grid(True)
+
+    plt.subplot(2, 2, 2)
+    # plt.plot(frames, upper_arm_lengths, color='blue', label='upper')
+    # plt.plot(frames, forearm_lengths, color='red', label='fore')
+    plt.plot(frames, wrix, color='blue', label='wrist')
+    plt.plot(frames, elbx, color='red', label='elbow')
+    plt.plot(frames, shox, color='green', label='shol')
+    plt.xlabel("Frame")
+    plt.ylabel("Angle (°)")
+    plt.title("Shoulder Abduction")
+    plt.grid(True)
+    plt.legend()
+
+    plt.subplot(2, 2, 3)
+    plt.plot(frames, elbow_angles, color='blue')
+    plt.xlabel("Frame")
+    plt.ylabel("Angle (°)")
+    plt.title("Elbow Angle")
+    plt.grid(True)
+
+    # plt.subplot(2, 2, 4)
+    # plt.plot(frames, lower_body_alignment, color='green')
+    # plt.xlabel("Frame")
+    # plt.ylabel("Angle (°)")
+    # plt.title("Lowbody Aligment")
+    # plt.grid(True)
+
+    plt.subplot(2, 2, 4)
+    plt.plot(frames, height1, color='green')
+    plt.xlabel("Frame")
+    plt.ylabel("pixel")
+    plt.title("카")
+    plt.grid(True)
+
+
+    plt.suptitle("Joint Angles Over Time")
+    plt.tight_layout(rect=[0, 0, 1, 0.95])
+    plt.show()
 
 if __name__ == "__main__":
-    video_path = os.path.join(os.getcwd(), "video.mp4")
+    video_path = os.path.join(os.getcwd(), "wide90.mp4")
     detect_and_display(video_path)
     analysis()
+    plot_joint_angles()
