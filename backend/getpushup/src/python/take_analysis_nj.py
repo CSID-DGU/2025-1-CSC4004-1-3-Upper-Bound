67a77065c221bcf98b5b10b6ebf50445c9ee9492
SeungMin-Woo <wsm020606@gmail.com>
2025-06-01 05:33:41 +0900

    chore : 원상복구, output_video 주소 변경 필요

--- backend/getpushup/src/python/take_analysis_nj.py
+++ backend/getpushup/src/python/take_analysis_nj.py
@@ -87,7 +87,9 @@ def detect_and_display(video_path, analysisId): # landmark 추출
     if rotate:
         width, height = height, width
 
-    output_dir = '/home/ubuntu/2025-1-CSC4004-1-3-Upper-Bound/backend/output_video/'  # 서버용 주소
+
+    output_dir = '../output_video/'
+    #output_dir = '/home/ubuntu/2025-1-CSC4004-1-3-Upper-Bound/backend/output_video/'  # 서버용 주소
     os.makedirs(output_dir, exist_ok=True)
     fourcc = cv2.VideoWriter_fourcc(*'avc1')  # 호환성 높임
     out = cv2.VideoWriter(output_dir + f'output{analysisId}.mp4', fourcc, fps, (width, height))
