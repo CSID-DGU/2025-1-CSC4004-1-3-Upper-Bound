2a6620459ac382396de1558fa312bdf3802aec2b
SeungMin-Woo <wsm020606@gmail.com>
2025-06-01 03:55:45 +0900

    chore : output_video 코덱 복구

--- backend/getpushup/src/python/take_analysis_nj.py
+++ backend/getpushup/src/python/take_analysis_nj.py
@@ -89,7 +89,7 @@ def detect_and_display(video_path, analysisId): # landmark 추출
 
     output_dir = '/home/ubuntu/2025-1-CSC4004-1-3-Upper-Bound/backend/output_video'
     os.makedirs(output_dir, exist_ok=True)
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # 호환성 높임
+    fourcc = cv2.VideoWriter_fourcc(*'avc1')  # 호환성 높임
     output_path = os.path.join(output_dir, f'output{analysisId}.mp4')
     out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
     
