8f4fbcba55108513c38ee541de5588060c3011d5
SeungMin-Woo <wsm020606@gmail.com>
2025-06-01 02:36:44 +0900

    chore : output_video 경로 수정 5...

--- backend/getpushup/src/python/take_analysis_nj.py
+++ backend/getpushup/src/python/take_analysis_nj.py
@@ -90,8 +90,10 @@ def detect_and_display(video_path, analysisId): # landmark 추출
     script_dir = os.path.dirname(os.path.abspath(__file__))  # 현재위치 backend/getpushup/src/python
     output_dir = os.path.abspath(os.path.join(script_dir, '..', '..', '..', '..', 'output_video'))
     os.makedirs(output_dir, exist_ok=True)
+
+    output_path = os.path.join(output_dir, f'output{analysisId}.mp4')
     fourcc = cv2.VideoWriter_fourcc(*'avc1')  # 호환성 높임
-    out = cv2.VideoWriter(output_dir + f'output{analysisId}.mp4', fourcc, fps, (width, height))
+    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
     
     # print("Saving to:", os.path.abspath('output_with_pose.mp4'))
     frame_idx = 0
