7c8d0f78be306a4429a804a10c216cbc63823a54
SeungMin-Woo <wsm020606@gmail.com>
2025-06-01 03:19:23 +0900

    chore : test

--- backend/getpushup/src/python/take_analysis_nj.py
+++ backend/getpushup/src/python/take_analysis_nj.py
@@ -87,10 +87,13 @@ def detect_and_display(video_path, analysisId): # landmark 추출
     if rotate:
         width, height = height, width
 
-    output_dir = '../output_video/'
+    script_dir = os.path.dirname(os.path.abspath(__file__))  # 현재 위치: getpushup/src/pushup/python
+    output_dir = os.path.abspath(os.path.join(script_dir, '..', '..', '..', 'output_video'))  # 루트 기준
+
     os.makedirs(output_dir, exist_ok=True)
-    fourcc = cv2.VideoWriter_fourcc(*'avc1')  # 호환성 높임
-    out = cv2.VideoWriter(output_dir + f'output{analysisId}.mp4', fourcc, fps, (width, height))
+    output_path = os.path.join(output_dir, f'output{analysisId}.mp4')
+    fourcc = cv2.VideoWriter_fourcc(*'avc1')
+    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
     
     # print("Saving to:", os.path.abspath('output_with_pose.mp4'))
     frame_idx = 0
